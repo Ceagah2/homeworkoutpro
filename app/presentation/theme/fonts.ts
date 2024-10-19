7337e12ed48a7c395b0414294465cd49f940f48d
Ceagah <carlosceagah@gmail.com>
2024-10-19 14:24:28 -0300

    style: update font token import

--- app/presentation/theme/fonts.ts
+++ app/presentation/theme/fonts.ts
@@ -1,10 +1,6 @@
-import { Montserrat_700Bold, Montserrat_400Regular } from "@expo-google-fonts/montserrat";
-import { Lato_400Regular, Lato_700Bold } from "@expo-google-fonts/lato"
-
-
 export const fonts = {
-  pageTitle: Lato_700Bold,
-  sectionTitle: Montserrat_700Bold,
-  body: Montserrat_400Regular,
-  description: Lato_400Regular,
+  heading: 'Lato_700Bold',
+  subTitle: 'Montserrat_700Bold',
+  body: 'Montserrat_400Regular',
+  description: 'Lato_400Regular',
 }
